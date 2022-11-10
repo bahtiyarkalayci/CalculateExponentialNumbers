fbb356ee27c23bf4dc1404c7e870da103e1c920a
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-10 20:14:08 +0300

    CalculateExponentialNumbers

--- src/ExponentialNumber.java
+++ src/ExponentialNumber.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ExponentialNumber {
+import java.util.Scanner;
+
+public class ExponentialNumber {
+    public static void main(String[] args) {
+       // n power p
+        int n,p,power=1;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the number=");
+        n= scanner.nextInt();
+        System.out.println("enter the power of number");
+        p=scanner.nextInt();
+
+        for (int i=1;i<=p;i++){
+            power=power*n;
+        }
+        System.out.println("power="+power);
+    }
 }
